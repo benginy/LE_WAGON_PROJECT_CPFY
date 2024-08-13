4f1d1b3d650a7907b009ce2f29f8e63915fcb601
Bengin Yilmaz <141424348+benginy@users.noreply.github.com>
2024-08-13 18:00:28 +0200

    Changes Day 2

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,18 @@ class BookingsController < ApplicationController
   def show
   end
 
+  def create
+    @life_moment = LifeMoment.find(params[:life_moment_id])
+    @booking = Booking.new(booking_params)
+    @booking.life_moment = @life_moment
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path, notice: 'Booking was successfully created.'
+    else
+      redirect_to @life_moment, alert: 'Unable to create booking.'
+    end
+  end
+
   def update
   end
 
@@ -12,5 +24,11 @@ class BookingsController < ApplicationController
   end
 
   def decline
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :booking_duration, :booking_cost, :status)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   def home
   end
 
   def dashboard
+    @bookings_made = Booking.where(user: current_user)
+
+    @published_life_moments = LifeMoment.where(user: current_user)
+
+    # Find bookings for these Life Moments
+    @bookings_sold = Booking.where(life_moment: @published_life_moments)
   end
 end
